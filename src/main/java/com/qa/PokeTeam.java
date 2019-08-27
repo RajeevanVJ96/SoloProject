4fa7248ec9b316d1cdecf203801adb7c3624b170
RajeevanVJ96 <rvijay46@gmail.com>
2019-08-27 18:04:12 +0100

    added initial obj files that I think I need

---
+++ src/main/java/com/qa/PokeTeam.java
@@ -0,0 +1,19 @@
+package com.qa;
+
+public class PokeTeam {
+
+    private String teamName;
+    private int capacity;
+
+    public String getTeamName() {
+        return teamName;
+    }
+
+    public PokeTeam(String teamName, int capacity){
+        this.teamName = teamName;
+        this.capacity = capacity;
+    }
+
+
+
+}
---
+++ src/main/java/com/qa/Pokemon.java
@@ -0,0 +1,30 @@
+package com.qa;
+
+public class Pokemon {
+
+    private int pokeNo;
+    private String name;
+    private int level;
+    private String nature;
+    private String m1;
+    private String m2;
+    private String m3;
+    private String m4;
+    private String description;
+
+    public Pokemon(int pokeNo, String name, int level, String nature, String m1, String m2, String m3, String m4, String description) {
+        this.pokeNo = pokeNo;
+        this.name = name;
+        this.level = level;
+        this.nature = nature;
+        this.m1 = m1;
+        this.m2 = m2;
+        this.m3 = m3;
+        this.m4 = m4;
+        this.description = description;
+    }
+
+    public int getPokeNo() {
+        return pokeNo;
+    }
+}
