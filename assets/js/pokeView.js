784c411f21a9b651ebf369c8d3043da9da97169e
Rajeevan <rvijay46@gmail.com>
2019-09-08 17:20:28 +0100

    working on taking in form input for making changes

--- assets/js/pokeView.js
+++ assets/js/pokeView.js
@@ -3,3 +3,13 @@ const imgsrc = localStorage.getItem('img');
 
 document.getElementById('h1').innerText = name
 document.getElementById('imgsrc').setAttribute('src', imgsrc);
+
+function handleDelete() {
+
+}
+
+function handleEdit() {
+    window.location = '/SoloProject/pokeEdit.html';
+    return false;
+}
+
